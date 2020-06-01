4cf22e912683a0270857159dab5477b7b5ad7753
Dejailson Silva <kauakarate@gmail.com>
2020-06-01 16:21:23 -0300

    aula7 finalizada

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aula7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cartao card = new Cartao();
+            card.AprovarCompra();
+
+            MasterCard master = new MasterCard();
+            master.parcelas = 10;
+            master.ComprarComDescontoMastercard(80f);
+
+        }
+    }
+}
